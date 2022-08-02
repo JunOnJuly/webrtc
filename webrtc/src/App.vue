fcb75476109e047d5ae7b865c02ca89cbc58118b
JunOnJuly <wlgnstjd0717@gmail.com>
2022-08-03 06:45:32 +0900

    update

---
+++ webrtc/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<video-comp>
+			
+		</video-comp>
+	</div>
+</template>
+
+<script>
+import VideoComp from './components/VideoComp.vue'
+
+export default {
+	components: {
+		VideoComp,
+	}
+}
+</script>
+
+<style>
+
+</style>
